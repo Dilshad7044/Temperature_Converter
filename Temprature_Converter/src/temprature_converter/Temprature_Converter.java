755477bf7ce44c10e6f1b4c2a2a84f88c1f9705d
DILSHAD ANSARI <84556373+Dilshad7044@users.noreply.github.com>
2023-07-06 04:07:16 +0530

    Add files via upload

---
+++ Temprature_Converter/src/temprature_converter/Temprature_Converter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temprature_converter;
+
+/**
+ *
+ * @author Dilshad Ansari
+ */
+public class Temprature_Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
